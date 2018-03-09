4c43b26dfc9c42edaa601667c0a0a7c8e593d373
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2018-03-09 13:34:11 +0100

    Add relationship users and groups

--- exercices/ex3/db/schema.rb
+++ exercices/ex3/db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309105852) do
+ActiveRecord::Schema.define(version: 20180309111034) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", limit: 50
+  end
+
+  create_table "groups_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "group_id", null: false
+    t.bigint "user_id", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
--- exercices/ex3/db/seeds.rb
+++ exercices/ex3/db/seeds.rb
@@ -2,14 +2,13 @@ require_relative '../connection'
 require_relative '../models/user'
 require_relative '../models/group'
 
-
-# Create some users
-user1 = User.create(firstname: 'Bernard', lastname: 'Margouille', email: 'bernard.margouille@reservoxx.ch')
-user2 = User.create(firstname: 'Marcel', lastname: 'Liota', email: 'marcel.liota@reservoxx.ch')
-user3 = User.create(firstname: 'Fabien', lastname: 'Carlotta', email: 'fabien.carlotta@reservoxx.ch')
-user4 = User.create(firstname: 'Sullyvan', lastname: 'Butoxx', email: 'sullyvan.butoxx@reservoxx.ch')
-
 # Create some groups
 group1 = Group.create(name: 'Admin')
 group2 = Group.create(name: 'Reservation')
 group3 = Group.create(name: 'Event')
+
+# Create some users
+user1 = User.create(firstname: 'Bernard', lastname: 'Margouille', email: 'bernard.margouille@reservoxx.ch', groups: [group1, group3])
+user2 = User.create(firstname: 'Marcel', lastname: 'Liota', email: 'marcel.liota@reservoxx.ch', groups: [group2, group3])
+user3 = User.create(firstname: 'Fabien', lastname: 'Carlotta', email: 'fabien.carlotta@reservoxx.ch', groups: [group3])
+user4 = User.create(firstname: 'Sullyvan', lastname: 'Butoxx', email: 'sullyvan.butoxx@reservoxx.ch', groups: [group1, group3])
